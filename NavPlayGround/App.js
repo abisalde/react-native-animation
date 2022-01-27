b6d3bdfcb7f47b9fe1568da751416558e5e3c585
abisalde <isaiah.abiodun@outlook.com>
2022-01-27 15:35:58 -0500

    CardView Test

--- NavPlayGround/App.js
+++ NavPlayGround/App.js
@@ -9,14 +9,25 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import CardView from 'react-native-cardview'
 import {NavigationContainer} from '@react-navigation/native';
 import Navigation from './routes';
 const App = () => {
   return (
     <NavigationContainer>
       <View style={styles.container}>
-        <Text>Hello World!</Text>
-        <Navigation />
+		<CardView
+		  style={{width: '50%', height: 100}}
+          cardElevation={3}
+          cardMaxElevation={5}
+          cornerRadius={5}>
+          <Text style={{alignItems: 'center',
+    paddingVertical: 20,
+    paddingHorizontal: 20,}}>
+              Elevation 0
+          </Text>
+		</CardView>
+        {/*<Navigation /> */}
       </View>
     </NavigationContainer>
   );
@@ -25,6 +36,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
   },
 });
 
