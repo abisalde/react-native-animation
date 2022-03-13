4bac59545c2beefe24000cc914fe80028b27eb67
abisalde <isaiah.abiodun@outlook.com>
2022-03-13 16:47:40 -0400

    Changes: Navigation, Camera testing

--- NavPlayGround/App.js
+++ NavPlayGround/App.js
@@ -13,6 +13,7 @@ import CardView from 'react-native-cardview';
 import {NavigationContainer} from '@react-navigation/native';
 import SwitchSelector from 'react-native-switch-selector';
 import Navigation from './routes';
+import CameraTest from './src/test/camera';
 // import AppTab from './src/curveBottomTab';
 // import {PermissionModal, PermissionItem} from 'react-native-permissions-modal';
 
@@ -27,75 +28,41 @@ const colors = {
 // }
 const App = () => {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <CardView
-          style={{width: '50%', height: 100}}
-          cardElevation={3}
-          cardMaxElevation={5}
-          cornerRadius={5}>
-          <Text
-            style={{
-              alignItems: 'center',
-              paddingVertical: 20,
-              paddingHorizontal: 20,
-            }}>
-            Elevation 0
-          </Text>
-        </CardView>
-        <SwitchSelector
-          initial={0}
-          onPress={value => console.log('The value is: ', value)}
-          textColor={colors.purple} //'#7a44cf'
-          selectedColor={colors.white}
-          buttonColor={colors.purple}
-          borderColor={colors.purple}
-          hasPadding
-          options={[
-            {label: 'Feminino', value: 'f'}, //images.feminino = require('./path_to/assets/img/feminino.png')
-            {label: 'Masculino', value: 'm'}, //images.masculino = require('./path_to/assets/img/masculino.png')
-          ]}
-        />
-        <Navigation />
-      </View>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <View style={styles.container}>
+    //     <CardView
+    //       style={{width: '50%', height: 100}}
+    //       cardElevation={3}
+    //       cardMaxElevation={5}
+    //       cornerRadius={5}>
+    //       <Text
+    //         style={{
+    //           alignItems: 'center',
+    //           paddingVertical: 20,
+    //           paddingHorizontal: 20,
+    //         }}>
+    //         Elevation 0
+    //       </Text>
+    //     </CardView>
+    //     <SwitchSelector
+    //       initial={0}
+    //       onPress={value => console.log('The value is: ', value)}
+    //       textColor={colors.purple} //'#7a44cf'
+    //       selectedColor={colors.white}
+    //       buttonColor={colors.purple}
+    //       borderColor={colors.purple}
+    //       hasPadding
+    //       options={[
+    //         {label: 'Feminino', value: 'f'}, //images.feminino = require('./path_to/assets/img/feminino.png')
+    //         {label: 'Masculino', value: 'm'}, //images.masculino = require('./path_to/assets/img/masculino.png')
+    //       ]}
+    //     />
+    //     <Navigation />
+    //   </View>
+    // </NavigationContainer>
+    <CameraTest />
   );
 };
-
-// const App = () => {
-//   const ref = React.useRef().permModal;
-//   return (
-//     <View style={styles.container}>
-//       <TouchableOpacity
-//         style={styles.buttonStyle}
-//         onPress={() => ref.openModal()}>
-//         <Text style={{color: '#31243f'}}>Open Permission Modal</Text>
-//       </TouchableOpacity>
-//       <PermissionModal
-//         panGestureEnabled={false}
-//         closeOnOverlayTap={false}
-//         ref={ref}>
-//         <PermissionItem />
-//         <PermissionItem
-//           title="Camera"
-//           iconStatusColor="red"
-//           subtitle="To access camera features"
-//           source={{
-//             uri: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
-//           }}
-//           onPress={() => alert('hello')}
-//         />
-//         <PermissionItem
-//           title="Internet"
-//           source={{
-//             uri: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
-//           }}
-//           subtitle="To access the services, give us the permission"
-//         />
-//       </PermissionModal>
-//     </View>
-//   );
-// };
 
 const styles = StyleSheet.create({
   container: {
--- NavPlayGround/src/test/camera.js
+++ NavPlayGround/src/test/camera.js
@@ -1,17 +1,37 @@
-// import React from 'react';
-// import {View, Text, ActivityIndicator, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, ActivityIndicator, StyleSheet} from 'react-native';
 
-// import {Camera, useCameraDevices} from 'react-native-vision-camera';
+import {Camera, useCameraDevices} from 'react-native-vision-camera';
 
-// const CameraTest = () => {
-//   const devices = useCameraDevices();
-//   const device = devices.back;
-//   if (device == null) {
-//     return <ActivityIndicator color="blue" />;
-//   }
-//   return (
-//     <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
-//   );
-// };
+const CameraTest = () => {
+  const [hasPermission, setHasPermission] = useState(false);
 
-// export default CameraTest;
+  useEffect(() => {
+    Camera.requestCameraPermission().then(status => {
+      if (status === 'authorized') {
+        setHasPermission(true);
+      }
+    });
+  }, []);
+
+  const devices = useCameraDevices();
+  const device = devices.back;
+
+  if (!hasPermission) {
+    return (
+      <View style={{flex: 1}}>
+        <Text>No permission</Text>
+      </View>
+    );
+  }
+
+  if (device == null) {
+    return <ActivityIndicator color="blue" />;
+  }
+
+  return (
+    <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+  );
+};
+
+export default CameraTest;
